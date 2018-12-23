a976cf7afe5d38131fc87c97aca3691f1067afcb
nyre <sangwulee2@gmail.com>
2018-12-22 22:41:25 -0500

    Adding Quill js for posting

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  handleChange = (value) => {
+    this.setState({ text: value });
+  };
+
+  render() {
+    return <ReactQuill value={this.state.text} onChange={this.handleChange} />;
+  }
+}
+
+export default PostForm;
